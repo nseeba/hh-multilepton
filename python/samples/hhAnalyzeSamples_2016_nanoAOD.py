25229e901aa6a9e928fbe2631339fbca094eb6e6
Karl Ehatäht <karlehataht@gmail.com>
2018-07-20 21:21:56 +0300

    Enable all HH->4tau samples by default

--- python/samples/hhAnalyzeSamples_2016_nanoAOD.py
+++ python/samples/hhAnalyzeSamples_2016_nanoAOD.py
@@ -16,7 +16,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-250_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   100000),
   ("fsize_local",                     143438765), # 143.44MB, avg file size 71.72MB
   ("fsize_db",                        3867669738), # 3.87GB, avg file size 276.26MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -53,7 +53,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-260_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   99204),
   ("fsize_local",                     143893312), # 143.89MB, avg file size 71.95MB
   ("fsize_db",                        3825455757), # 3.83GB, avg file size 239.09MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -90,7 +90,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-270_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   100000),
   ("fsize_local",                     148796541), # 148.80MB, avg file size 49.60MB
   ("fsize_db",                        3828027078), # 3.83GB, avg file size 255.20MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -127,7 +127,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-280_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   98567),
   ("fsize_local",                     145319801), # 145.32MB, avg file size 72.66MB
   ("fsize_db",                        3819426599), # 3.82GB, avg file size 181.88MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -164,7 +164,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-300_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   88976),
   ("fsize_local",                     133818598), # 133.82MB, avg file size 66.91MB
   ("fsize_db",                        3475966865), # 3.48GB, avg file size 193.11MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -201,7 +201,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-320_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   100000),
   ("fsize_local",                     154748743), # 154.75MB, avg file size 51.58MB
   ("fsize_db",                        3892930646), # 3.89GB, avg file size 324.41MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -238,7 +238,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-340_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   100000),
   ("fsize_local",                     157094753), # 157.09MB, avg file size 52.36MB
   ("fsize_db",                        3947815706), # 3.95GB, avg file size 281.99MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -275,7 +275,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-350_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   98356),
   ("fsize_local",                     153120130), # 153.12MB, avg file size 76.56MB
   ("fsize_db",                        3939843876), # 3.94GB, avg file size 179.08MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -312,7 +312,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-400_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   97614),
   ("fsize_local",                     158095964), # 158.10MB, avg file size 79.05MB
   ("fsize_db",                        3967336020), # 3.97GB, avg file size 208.81MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -349,7 +349,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-450_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   100000),
   ("fsize_local",                     167144265), # 167.14MB, avg file size 83.57MB
   ("fsize_db",                        4142163410), # 4.14GB, avg file size 197.25MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -386,7 +386,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-500_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   50000),
   ("fsize_local",                     89568771), # 89.57MB, avg file size 44.78MB
   ("fsize_db",                        2135623539), # 2.14GB, avg file size 142.37MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -423,7 +423,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-550_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   50000),
   ("fsize_local",                     89813654), # 89.81MB, avg file size 89.81MB
   ("fsize_db",                        2093059468), # 2.09GB, avg file size 418.61MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -460,7 +460,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-600_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   50000),
   ("fsize_local",                     94762521), # 94.76MB, avg file size 47.38MB
   ("fsize_db",                        2163109704), # 2.16GB, avg file size 180.26MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -497,7 +497,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-650_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   47684),
   ("fsize_local",                     90245004), # 90.25MB, avg file size 90.25MB
   ("fsize_db",                        2147010667), # 2.15GB, avg file size 107.35MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -534,7 +534,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-700_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   50000),
   ("fsize_local",                     99286024), # 99.29MB, avg file size 49.64MB
   ("fsize_db",                        2213964233), # 2.21GB, avg file size 201.27MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -571,7 +571,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-750_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   49280),
   ("fsize_local",                     97213670), # 97.21MB, avg file size 97.21MB
   ("fsize_db",                        2195753227), # 2.20GB, avg file size 199.61MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -608,7 +608,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-800_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   50000),
   ("fsize_local",                     100085683), # 100.09MB, avg file size 100.09MB
   ("fsize_db",                        2222020975), # 2.22GB, avg file size 277.75MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
@@ -645,7 +645,7 @@ samples_2016["/GluGluToRadionToHHTo4Tau_M-900_narrow_13TeV-madgraph/RunIISummer1
   ("nof_db_events",                   50000),
   ("fsize_local",                     105085137), # 105.09MB, avg file size 52.54MB
   ("fsize_db",                        2320248894), # 2.32GB, avg file size 128.90MB
-  ("use_it",                          False),
+  ("use_it",                          True),
   ("xsection",                        1.0),
   ("genWeight",                       True),
   ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
--- python/samples/metaDict_2016_hh.py
+++ python/samples/metaDict_2016_hh.py
@@ -13,7 +13,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-250_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          14),
   ("fsize_db",              3867669738),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 3.87GB; nevents: 100.00k; release: 8_0_21; last modified: 2017-05-26 13:45:14"),
 ])
@@ -26,7 +26,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-260_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          16),
   ("fsize_db",              3825455757),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 3.83GB; nevents: 99.20k; release: 8_0_21; last modified: 2017-05-26 13:43:34"),
 ])
@@ -39,7 +39,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-270_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          15),
   ("fsize_db",              3828027078),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 3.83GB; nevents: 100.00k; release: 8_0_21; last modified: 2017-05-26 23:45:52"),
 ])
@@ -52,7 +52,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-280_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          21),
   ("fsize_db",              3819426599),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 3.82GB; nevents: 98.57k; release: 8_0_21; last modified: 2017-05-27 08:14:20"),
 ])
@@ -65,7 +65,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-300_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          18),
   ("fsize_db",              3475966865),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 3.48GB; nevents: 88.98k; release: 8_0_21; last modified: 2017-06-07 01:45:51"),
 ])
@@ -78,7 +78,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-320_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          12),
   ("fsize_db",              3892930646),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 3.89GB; nevents: 100.00k; release: 8_0_21; last modified: 2017-05-27 08:17:05"),
 ])
@@ -91,7 +91,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-340_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          14),
   ("fsize_db",              3947815706),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 3.95GB; nevents: 100.00k; release: 8_0_21; last modified: 2017-05-26 23:57:54"),
 ])
@@ -104,7 +104,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-350_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          22),
   ("fsize_db",              3939843876),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 3.94GB; nevents: 98.36k; release: 8_0_21; last modified: 2017-05-27 17:54:45"),
 ])
@@ -117,7 +117,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-400_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          19),
   ("fsize_db",              3967336020),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 3.97GB; nevents: 97.61k; release: 8_0_21; last modified: 2017-05-28 01:35:13"),
 ])
@@ -130,7 +130,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-450_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          21),
   ("fsize_db",              4142163410),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 4.14GB; nevents: 100.00k; release: 8_0_21; last modified: 2017-05-26 23:48:36"),
 ])
@@ -143,7 +143,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-500_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          15),
   ("fsize_db",              2135623539),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 2.14GB; nevents: 50.00k; release: 8_0_21; last modified: 2017-05-26 13:37:37"),
 ])
@@ -156,7 +156,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-550_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          5),
   ("fsize_db",              2093059468),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 2.09GB; nevents: 50.00k; release: 8_0_21; last modified: 2017-05-25 22:35:10"),
 ])
@@ -169,7 +169,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-600_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          12),
   ("fsize_db",              2163109704),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 2.16GB; nevents: 50.00k; release: 8_0_21; last modified: 2017-05-26 13:27:57"),
 ])
@@ -182,7 +182,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-650_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          20),
   ("fsize_db",              2147010667),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 2.15GB; nevents: 47.68k; release: 8_0_21; last modified: 2017-05-26 23:53:54"),
 ])
@@ -195,7 +195,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-700_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          11),
   ("fsize_db",              2213964233),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 2.21GB; nevents: 50.00k; release: 8_0_21; last modified: 2017-05-27 00:00:33"),
 ])
@@ -208,7 +208,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-750_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          11),
   ("fsize_db",              2195753227),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 2.20GB; nevents: 49.28k; release: 8_0_21; last modified: 2017-05-26 13:32:57"),
 ])
@@ -221,7 +221,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-800_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          8),
   ("fsize_db",              2222020975),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 2.22GB; nevents: 50.00k; release: 8_0_21; last modified: 2017-05-27 00:08:49"),
 ])
@@ -234,7 +234,7 @@ meta_dictionary["/GluGluToRadionToHHTo4Tau_M-900_narrow_13TeV-madgraph/RunIISumm
   ("nof_db_files",          18),
   ("fsize_db",              2320248894),
   ("xsection",              1.0),
-  ("use_it",                False),
+  ("use_it",                True),
   ("genWeight",             True),
   ("comment",               "status: VALID; size: 2.32GB; nevents: 50.00k; release: 8_0_21; last modified: 2017-05-27 08:10:44"),
 ])
