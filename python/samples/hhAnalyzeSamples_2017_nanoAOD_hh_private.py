4e25f91f1248879e7a42238bb100dcaed54a496f
Karl Ehatäht <karlehataht@gmail.com>
2018-08-04 19:46:05 +0300

    Update meta & sample dict of HH samples for 2017 era

--- python/samples/hhAnalyzeSamples_2017_nanoAOD_hh_private.py
+++ python/samples/hhAnalyzeSamples_2017_nanoAOD_hh_private.py
@@ -1,13 +1,13 @@
 from collections import OrderedDict as OD
 
-# file generated at 2018-08-03 20:22:51 with the following command:
+# file generated at 2018-08-04 19:43:43 with the following command:
 # create_dictionary.py -m python/samples/metaDict_2017_private_hh.py -p /hdfs/cms/store/user/kaehatah/NanoProduction_v2_2018Aug03 -Z zeroes.txt -z zombies.txt -N samples_2017 -E 2017 -g hhAnalyzeSamples_2017_nanoAOD_hh_private.py -o python/samples -M
 
 samples_2017 = OD()
 samples_2017["/HHTo4T_madgraph_pythia8_CP5_M400/private/USER"] = OD([
   ("type",                            "mc"),
   ("sample_category",                 "signal"),
-  ("process_name_specific",           "hh_4t_400"),
+  ("process_name_specific",           "signal_hh_4t_400"),
   ("nof_files",                       193),
   ("nof_db_files",                    193),
   ("nof_events",                      {
@@ -44,7 +44,7 @@ samples_2017["/HHTo4T_madgraph_pythia8_CP5_M400/private/USER"] = OD([
 samples_2017["/HHTo4T_madgraph_pythia8_CP5_M700/private/USER"] = OD([
   ("type",                            "mc"),
   ("sample_category",                 "signal"),
-  ("process_name_specific",           "hh_4t_700"),
+  ("process_name_specific",           "signal_hh_4t_700"),
   ("nof_files",                       160),
   ("nof_db_files",                    160),
   ("nof_events",                      {
@@ -62,6 +62,154 @@ samples_2017["/HHTo4T_madgraph_pythia8_CP5_M700/private/USER"] = OD([
     [
       OD([
         ("path",      "/hdfs/cms/store/user/kaehatah/NanoProduction_v2_2018Aug03/HHTo4T_madgraph_pythia8_CP5_M700/NanoProduction_v2_2018Aug03_HHTo4T_madgraph_pythia8_CP5_M700__private/000000_000000"),
+        ("selection", "*"),
+        ("blacklist", []),
+      ]),
+    ]
+  ),
+  ("missing_from_superset",           [
+    # not computed
+  ]),
+  ("missing_hlt_paths",               [
+
+  ]),
+  ("hlt_paths",               [
+    # not computed
+  ]),
+])
+
+samples_2017["/HHTo2T2V_madgraph_pythia8_CP5_M400/private/USER"] = OD([
+  ("type",                            "mc"),
+  ("sample_category",                 "signal"),
+  ("process_name_specific",           "signal_hh_2t2v_400"),
+  ("nof_files",                       130),
+  ("nof_db_files",                    130),
+  ("nof_events",                      {
+  }),
+  ("nof_tree_events",                 257207),
+  ("nof_db_events",                   257207),
+  ("fsize_local",                     822681731), # 822.68MB, avg file size 6.33MB
+  ("fsize_db",                        15401525854), # 15.40GB, avg file size 118.47MB
+  ("use_it",                          True),
+  ("xsection",                        0.030092),
+  ("genWeight",                       True),
+  ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
+  ("has_LHE",                         True),
+  ("local_paths",
+    [
+      OD([
+        ("path",      "/hdfs/cms/store/user/kaehatah/NanoProduction_v2_2018Aug03/HHTo2T2V_madgraph_pythia8_CP5_M400/NanoProduction_v2_2018Aug03_HHTo2T2V_madgraph_pythia8_CP5_M400__private/000000_000000"),
+        ("selection", "*"),
+        ("blacklist", []),
+      ]),
+    ]
+  ),
+  ("missing_from_superset",           [
+    # not computed
+  ]),
+  ("missing_hlt_paths",               [
+
+  ]),
+  ("hlt_paths",               [
+    # not computed
+  ]),
+])
+
+samples_2017["/HHTo2T2V_madgraph_pythia8_CP5_M700/private/USER"] = OD([
+  ("type",                            "mc"),
+  ("sample_category",                 "signal"),
+  ("process_name_specific",           "signal_hh_2t2v_700"),
+  ("nof_files",                       152),
+  ("nof_db_files",                    152),
+  ("nof_events",                      {
+  }),
+  ("nof_tree_events",                 301884),
+  ("nof_db_events",                   301884),
+  ("fsize_local",                     1073676334), # 1.07GB, avg file size 7.06MB
+  ("fsize_db",                        19508012725), # 19.51GB, avg file size 128.34MB
+  ("use_it",                          True),
+  ("xsection",                        0.030092),
+  ("genWeight",                       True),
+  ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
+  ("has_LHE",                         True),
+  ("local_paths",
+    [
+      OD([
+        ("path",      "/hdfs/cms/store/user/kaehatah/NanoProduction_v2_2018Aug03/HHTo2T2V_madgraph_pythia8_CP5_M700/NanoProduction_v2_2018Aug03_HHTo2T2V_madgraph_pythia8_CP5_M700__private/000000_000000"),
+        ("selection", "*"),
+        ("blacklist", []),
+      ]),
+    ]
+  ),
+  ("missing_from_superset",           [
+    # not computed
+  ]),
+  ("missing_hlt_paths",               [
+
+  ]),
+  ("hlt_paths",               [
+    # not computed
+  ]),
+])
+
+samples_2017["/HHTo4V_madgraph_pythia8_CP5_M400/private/USER"] = OD([
+  ("type",                            "mc"),
+  ("sample_category",                 "signal"),
+  ("process_name_specific",           "signal_hh_4v_400"),
+  ("nof_files",                       133),
+  ("nof_db_files",                    133),
+  ("nof_events",                      {
+  }),
+  ("nof_tree_events",                 262996),
+  ("nof_db_events",                   262996),
+  ("fsize_local",                     850207785), # 850.21MB, avg file size 6.39MB
+  ("fsize_db",                        16090082982), # 16.09GB, avg file size 120.98MB
+  ("use_it",                          True),
+  ("xsection",                        0.057547),
+  ("genWeight",                       True),
+  ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
+  ("has_LHE",                         True),
+  ("local_paths",
+    [
+      OD([
+        ("path",      "/hdfs/cms/store/user/kaehatah/NanoProduction_v2_2018Aug03/HHTo4V_madgraph_pythia8_CP5_M400/NanoProduction_v2_2018Aug03_HHTo4V_madgraph_pythia8_CP5_M400__private/000000_000000"),
+        ("selection", "*"),
+        ("blacklist", [1]),
+      ]),
+    ]
+  ),
+  ("missing_from_superset",           [
+    # not computed
+  ]),
+  ("missing_hlt_paths",               [
+
+  ]),
+  ("hlt_paths",               [
+    # not computed
+  ]),
+])
+
+samples_2017["/HHTo4V_madgraph_pythia8_CP5_M700/private/USER"] = OD([
+  ("type",                            "mc"),
+  ("sample_category",                 "signal"),
+  ("process_name_specific",           "signal_hh_4v_700"),
+  ("nof_files",                       118),
+  ("nof_db_files",                    118),
+  ("nof_events",                      {
+  }),
+  ("nof_tree_events",                 230116),
+  ("nof_db_events",                   230116),
+  ("fsize_local",                     839448992), # 839.45MB, avg file size 7.11MB
+  ("fsize_db",                        15395786053), # 15.40GB, avg file size 130.47MB
+  ("use_it",                          True),
+  ("xsection",                        0.057547),
+  ("genWeight",                       True),
+  ("triggers",                        ['1e', '1mu', '2e', '2mu', '1e1mu', '3e', '3mu', '2e1mu', '1e2mu', '1e1tau', '1mu1tau', '2tau']),
+  ("has_LHE",                         True),
+  ("local_paths",
+    [
+      OD([
+        ("path",      "/hdfs/cms/store/user/kaehatah/NanoProduction_v2_2018Aug03/HHTo4V_madgraph_pythia8_CP5_M700/NanoProduction_v2_2018Aug03_HHTo4V_madgraph_pythia8_CP5_M700__private/000000_000000"),
         ("selection", "*"),
         ("blacklist", []),
       ]),
