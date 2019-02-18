76e3ffd4896b2403dd4dc0c91b31aa516f698492
Ram <ram.krishna.dewanjee@cern.ch>
2019-02-18 11:35:30 +0200

    added missing samples to BDT training

--- python/samples/hhAnalyzeSamples_2017_BDT_2l_2tau.py
+++ python/samples/hhAnalyzeSamples_2017_BDT_2l_2tau.py
@@ -6,9 +6,13 @@ for sample_name, sample_info in samples_2017.items():
   if sample_name == 'sum_events':
     continue
   if sample_info["process_name_specific"] in [
+        "ttHJetToNonbb_M125_amcatnlo",
         "ttHToNonbb_M125_powheg",
+        "ttHToNonbb_M125_powheg_ext1",
         "TTZJets_LO",
+        "TTZJets_LO_ext1",
         "TTWJets_LO",
+        "TTWJets_LO_ext1",
         "TTTo2L2Nu",
         "TTTo2L2Nu_PSweights",
         "TTToSemiLeptonic",
