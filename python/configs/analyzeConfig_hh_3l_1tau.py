81ba26facffbc4ced1d8ba42f7ea34700c2d1499
Christian Veelken <christian.veelken@cern.ch>
2019-04-17 12:15:50 +0300

    bug-fix to handle use-case of BDT training Ntuple production, which broke when switching to phony targets in Makefile (to reduce load on /hdfs arising from checking existence of all output ROOT files)

--- python/configs/analyzeConfig_hh_3l_1tau.py
+++ python/configs/analyzeConfig_hh_3l_1tau.py
@@ -435,9 +435,6 @@ class analyzeConfig_hh_3l_1tau(analyzeConfig_hh):
                 self.outputFile_hadd_stage1[key_hadd_stage1_job] = os.path.join(self.dirs[key_hadd_stage1_dir][DKEY_HIST],
                                                                                 "hadd_stage1_%s_%s_%s.root" % hadd_stage1_job_tuple)
 
-                if self.isBDTtraining:
-                  self.targets.append(self.outputFile_hadd_stage1[key_hadd_stage1])
-
             if self.isBDTtraining:
               continue
 
@@ -621,6 +618,7 @@ class analyzeConfig_hh_3l_1tau(analyzeConfig_hh):
       lines_makefile = []
       self.addToMakefile_analyze(lines_makefile)
       self.addToMakefile_hadd_stage1(lines_makefile)
+      self.targets.extend(self.phoniesToAdd)
       self.createMakefile(lines_makefile)
       logging.info("Done")
       return self.num_jobs
