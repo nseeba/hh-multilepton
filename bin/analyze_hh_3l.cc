3c5c970cddc62f9b136abbbd885ac23ba6dd63b8
Christian Veelken <christian.veelken@cern.ch>
2019-05-15 16:29:33 +0300

    fixed compilation errors

--- bin/analyze_hh_3l.cc
+++ bin/analyze_hh_3l.cc
@@ -131,7 +131,7 @@ void dumpGenParticles(const std::string& label, const std::vector<GenParticle>& 
   }
 }
 
-void printWjj(const std::vector<const RecoJetAK8*>& jets_ak8, const RecoJetCollectionSelectorAK8_bbWW_Wjj& jetSelectorAK8_Wjj, 
+void printWjj(const std::vector<const RecoJetAK8*>& jets_ak8, const RecoJetCollectionSelectorAK8_hh_Wjj& jetSelectorAK8_Wjj, 
 	      const std::vector<GenParticle>& genWBosons, const std::vector<GenParticle>& genWJets)
 {
   std::cout << "<printWjj>:" << std::endl;
@@ -522,7 +522,7 @@ int main(int argc, char* argv[])
   RecoJetCollectionCleanerAK8 jetCleanerAK8_dR08(0.8, isDEBUG);
   RecoJetCollectionCleanerAK8 jetCleanerAK8_dR12(1.2, isDEBUG);
   RecoJetCollectionCleanerAK8 jetCleanerAK8_dR16(1.6, isDEBUG);
-  RecoJetCollectionSelectorAK8_bbWW_Wjj jetSelectorAK8_Wjj(era, -1, isDEBUG);
+  RecoJetCollectionSelectorAK8_hh_Wjj jetSelectorAK8_Wjj(era, -1, isDEBUG);
   
   //--- declare missing transverse energy
   RecoMEtReader* metReader = new RecoMEtReader(era, isMC, branchName_met);
