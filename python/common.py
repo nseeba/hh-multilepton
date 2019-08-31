9d12213e7ba680aa28a3f769e6db9e661d26b890
ktht <karlehataht@gmail.com>
2019-08-31 15:32:03 +0300

    Fix filtering of non-resonant samples

--- python/common.py
+++ python/common.py
@@ -1,10 +1,6 @@
 import re
 
-NONRESONANT_RE    = re.compile(r'signal_ggf_nonresonant_\w+')
-NONRESONANT_SM_RE = re.compile(r'signal_ggf_nonresonant_node_sm_hh_\w+')
+NONRESONANT_RE = re.compile(r'signal_ggf_nonresonant_\w+')
 
 def is_nonresonant(sample_category, require_sm = False):
-  if require_sm:
-    return bool(NONRESONANT_SM_RE.match(sample_category))
-  else:
-    return bool(NONRESONANT_RE.match(sample_category))
+  return bool(NONRESONANT_RE.match(sample_category))
