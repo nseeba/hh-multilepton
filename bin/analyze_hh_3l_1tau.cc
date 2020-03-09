f8a31c29e0ec1c6239d0621c6b43c8d17304747d
ktht <karlehataht@gmail.com>
2020-03-09 16:21:11 +0200

    Update the usage of compute_FR_1tau

--- bin/analyze_hh_3l_1tau.cc
+++ bin/analyze_hh_3l_1tau.cc
@@ -1441,7 +1441,7 @@ int main(int argc, char* argv[])
     }
     else if(applyFakeRateWeights == kFR_1tau)
     {
-      evtWeightRecorder.compute_FR_1tau();
+      evtWeightRecorder.compute_FR_1tau(passesTight_hadTau);
     }
     
     // CV: apply data/MC ratio for jet->tau fake-rates in case data-driven "fake" background estimation is applied to leptons only
