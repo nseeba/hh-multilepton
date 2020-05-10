a015be7a15fb35dea8bb58b511a7475b30aa78d1
ktht <karlehataht@gmail.com>
2020-05-11 01:32:10 +0300

    Do not consider NLO non-resonant samples where it's not needed

--- python/common.py
+++ python/common.py
@@ -2,5 +2,7 @@ import re
 
 NONRESONANT_RE = re.compile(r'signal_ggf_nonresonant_\w+')
 
-def is_nonresonant(sample_category, require_sm = False):
+def is_nonresonant(sample_category, allow_nlo = False):
+  if not allow_nlo and 'cHHH' in sample_category:
+    return False
   return bool(NONRESONANT_RE.match(sample_category))
