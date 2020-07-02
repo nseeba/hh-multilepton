826c48c226b89aa21921c70fba7993f34c7b58e3
ktht <karlehataht@gmail.com>
2020-07-02 11:52:25 +0300

    Sample dictionary specifically for BDT training
    https://github.com/HEP-KBFI/hh-multilepton/issues/10

---
+++ python/samples/hhAnalyzeSamples_2016_BDT.py
@@ -0,0 +1,16 @@
+from hhAnalysis.multilepton.samples.hhAnalyzeSamples_2016 import samples_2016
+from tthAnalysis.HiggsToTauTau.common import load_samples_stitched
+
+samples_2016 = load_samples_stitched(samples_2016, '2016', load_dy = True, load_wjets = True) # load LO DY and W+jets
+
+for sample_name, sample_info in samples_2016.items():
+  if sample_name == 'sum_events':
+    continue
+  process_name = sample_info["process_name_specific"]
+  if process_name.startswith('WZTo3LNu'):
+    # or maybe even use samples binned by jet multiplicity
+    sample_info["use_it"] = 'powheg' in process_name
+  elif process_name.lower().startswith('ttz'):
+    sample_info["use_it"] = 'TTZJets_LO' in process_name
+  elif process_name.lower().startswith('ttw'):
+    sample_info["use_it"] = 'TTWJets_LO' in process_name
