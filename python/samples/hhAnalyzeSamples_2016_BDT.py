b36a5ffa4a29dd43cfe2bf7e9e8e0741fea55e7e
Tobias Kramer <tobias.kramer@desy.de>
2020-08-05 19:06:54 +0300

    Fixed processing WZTo3LNu_mllmin01 samples for BDT NTuples

--- python/samples/hhAnalyzeSamples_2016_BDT.py
+++ python/samples/hhAnalyzeSamples_2016_BDT.py
@@ -8,11 +8,8 @@ for sample_name, sample_info in samples_2016.items():
     continue
   process_name = sample_info["process_name_specific"]
   if process_name.startswith('WZTo3LNu'):
-    # or maybe even use samples binned by jet multiplicity
-    sample_info["use_it"] = 'powheg' in process_name
+    sample_info["use_it"] = process_name.startswith(('WZTo3LNu_powheg', 'WZTo3LNu_mllmin01'))
   elif process_name.lower().startswith('ttz'):
     sample_info["use_it"] = 'TTZJets_LO' in process_name
   elif process_name.lower().startswith('ttw'):
     sample_info["use_it"] = 'TTWJets_LO' in process_name
-  elif process_name.startswith('WZTo3LNu'):
-    sample_info["use_it"] = process_name.startswith(('WZTo3LNu_powheg', 'WZTo3LNu_mllmin01'))
