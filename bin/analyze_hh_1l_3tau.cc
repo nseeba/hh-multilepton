ab30e870d019c96b03c6ef9709ecae42457c11aa
saswatinandan <saswati.nandan@cern.ch>
2020-09-07 18:27:58 +0300

    applying eventWeight to reWeightMapHH for non-resonant case

--- bin/analyze_hh_1l_3tau.cc
+++ bin/analyze_hh_1l_3tau.cc
@@ -1787,16 +1787,26 @@ int main(int argc, char* argv[])
         }
         std::cout << '\n';
       }
-    } 
-    else 
+    }
+
+    for(const std::string & central_or_shift: central_or_shifts_local)
     {
-      for(const std::string & evt_cat_str: evt_cat_strs) 
+      const double evtWeight = evtWeightRecorder.get(central_or_shift);
+      const bool skipFilling = central_or_shift != central_or_shift_main;
+      for(const std::string & evt_cat_str: evt_cat_strs)
       {
-	if(evt_cat_str != default_cat_str)
+	if(skipFilling && evt_cat_str != default_cat_str)
 	{
 	  continue;
 	}
-	reWeightMapHH[evt_cat_str] = evtWeightRecorder.get(central_or_shift_main);
+	if(apply_HH_rwgt)
+	{
+	  reWeightMapHH[evt_cat_str] *= evtWeight;
+	}
+	else
+	{
+	  reWeightMapHH[evt_cat_str] = evtWeight;
+	}
       }
     }
     
--- bin/analyze_hh_2l_2tau.cc
+++ bin/analyze_hh_2l_2tau.cc
@@ -1726,18 +1726,29 @@ int main(int argc, char* argv[])
 
         std::cout << '\n';
       }
-    } else { // Siddesh lines
-      for(const std::string & evt_cat_str: evt_cat_strs) {
-	if(evt_cat_str != default_cat_str)
-	  {
-	    continue;
-	  }
-	
-	reWeightMapHH[evt_cat_str] = evtWeightRecorder.get(central_or_shift_main);
-	std::cout << "\t evt_cat_str: " << evt_cat_str << ", reWeightMapHH: " << reWeightMapHH[evt_cat_str] << std::endl;
+    } 
+
+    for(const std::string & central_or_shift: central_or_shifts_local)
+    {
+      const double evtWeight = evtWeightRecorder.get(central_or_shift);
+      const bool skipFilling = central_or_shift != central_or_shift_main;
+      for(const std::string & evt_cat_str: evt_cat_strs)
+      {
+	if(skipFilling && evt_cat_str != default_cat_str)
+	{
+	  continue;
+	}
+	if(apply_HH_rwgt)
+	{
+	  reWeightMapHH[evt_cat_str] *= evtWeight;
+	}
+	else
+	{
+	  reWeightMapHH[evt_cat_str] = evtWeight;
+	}
       }
     }
-
+    
     std::cout<<"reWeightMapHH.size() " << reWeightMapHH.size() << std::endl;
 
     const Particle::LorentzVector tautau_p4 = selHadTau_lead->p4() + selHadTau_sublead->p4();
