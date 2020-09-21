f47946e73d813921868f6e5977ba0157138c8dd0
ktht <karlehataht@gmail.com>
2020-09-21 15:29:37 +0300

    Update meta dictionaries
    https://github.com/HEP-KBFI/hh-multilepton/issues/19

---
+++ python/samples/metaDict_2017_private.py
@@ -0,0 +1,137 @@
+from collections import OrderedDict as OD
+
+# file generated at 2020-09-21 13:15:40 with the following command:
+# find_samples.py -V -i ../../tthAnalysis/NanoAOD/test/datasets/txt/datasets_hh_multilepton_mc_2017_RunIIFall17MiniAODv2_private.txt -m python/samples/metaDict_2017_private.py -c python/samples/sampleLocations_2017_nanoAOD_hh_multilepton.txt
+
+meta_dictionary = OD()
+
+
+### event sums
+
+sum_events = { 
+}
+
+
+meta_dictionary["/GluGluToHHTo4Tau_node_SM_13TeV/RunIIFall17MiniAODv2/USER"] =  OD([
+  ("crab_string",           ""),
+  ("sample_category",       "signal_ggf_nonresonant_hh_tttt"),
+  ("process_name_specific", "signal_ggf_nonresonant_node_sm_hh_4t_private"),
+  ("nof_db_events",         1000000),
+  ("nof_db_files",          4000),
+  ("fsize_db",              79886965724),
+  ("xsection",              0.003934),
+  ("use_it",                True),
+  ("genWeight",             True),
+  ("comment",               "status: VALID; size: 79.89GB; nevents: 1.00M; release: 9_4_0; last modified: 2020-09-20 20:38:06"),
+])
+
+meta_dictionary["/GluGluToHHTo4Tau_node_1_TuneCP5_PSWeights_13TeV/RunIIFall17MiniAODv2/USER"] =  OD([
+  ("crab_string",           ""),
+  ("sample_category",       "signal_ggf_nonresonant_hh_tttt"),
+  ("process_name_specific", "signal_ggf_nonresonant_node_1_hh_4t_private"),
+  ("nof_db_events",         1000000),
+  ("nof_db_files",          4000),
+  ("fsize_db",              79103654607),
+  ("xsection",              0.003934),
+  ("use_it",                True),
+  ("genWeight",             True),
+  ("comment",               "status: VALID; size: 79.10GB; nevents: 1.00M; release: 9_4_0; last modified: 2020-09-20 15:05:40"),
+])
+
+meta_dictionary["/GluGluToHHTo4Tau_node_7_13TeV/RunIIFall17MiniAODv2/USER"] =  OD([
+  ("crab_string",           ""),
+  ("sample_category",       "signal_ggf_nonresonant_hh_tttt"),
+  ("process_name_specific", "signal_ggf_nonresonant_node_7_hh_4t_private"),
+  ("nof_db_events",         1000000),
+  ("nof_db_files",          4000),
+  ("fsize_db",              79131507440),
+  ("xsection",              0.003934),
+  ("use_it",                True),
+  ("genWeight",             True),
+  ("comment",               "status: VALID; size: 79.13GB; nevents: 1.00M; release: 9_4_0; last modified: 2020-09-20 17:11:06"),
+])
+
+meta_dictionary["/GluGluToHHTo2V2Tau_node_SM_13TeV/RunIIFall17MiniAODv2/USER"] =  OD([
+  ("crab_string",           ""),
+  ("sample_category",       "signal_ggf_nonresonant_hh_wwtt"),
+  ("process_name_specific", "signal_ggf_nonresonant_node_sm_hh_2v2t_private"),
+  ("nof_db_events",         999997),
+  ("nof_db_files",          4000),
+  ("fsize_db",              81657974168),
+  ("xsection",              0.030092),
+  ("use_it",                True),
+  ("genWeight",             True),
+  ("comment",               "status: VALID; size: 81.66GB; nevents: 1000.00k; release: 9_4_0; last modified: 2020-09-20 20:38:28"),
+])
+
+meta_dictionary["/GluGluToHHTo2V2tau_node_1_TuneCP5_PSWeights_13TeV/RunIIFall17MiniAODv2/USER"] =  OD([
+  ("crab_string",           ""),
+  ("sample_category",       "signal_ggf_nonresonant_hh_wwtt"),
+  ("process_name_specific", "signal_ggf_nonresonant_node_1_hh_2v2t_private"),
+  ("nof_db_events",         999998),
+  ("nof_db_files",          4000),
+  ("fsize_db",              80767326118),
+  ("xsection",              0.030092),
+  ("use_it",                True),
+  ("genWeight",             True),
+  ("comment",               "status: VALID; size: 80.77GB; nevents: 1000.00k; release: 9_4_0; last modified: 2020-09-20 15:05:07"),
+])
+
+meta_dictionary["/GluGluToHHTo2V2Tau_node_7_13TeV/RunIIFall17MiniAODv2/USER"] =  OD([
+  ("crab_string",           ""),
+  ("sample_category",       "signal_ggf_nonresonant_hh_wwtt"),
+  ("process_name_specific", "signal_ggf_nonresonant_node_7_hh_2v2t_private"),
+  ("nof_db_events",         999998),
+  ("nof_db_files",          4000),
+  ("fsize_db",              80792256740),
+  ("xsection",              0.030092),
+  ("use_it",                True),
+  ("genWeight",             True),
+  ("comment",               "status: VALID; size: 80.79GB; nevents: 1000.00k; release: 9_4_0; last modified: 2020-09-20 18:49:30"),
+])
+
+meta_dictionary["/GluGluToHHTo4V_node_SM_13TeV/RunIIFall17MiniAODv2/USER"] =  OD([
+  ("crab_string",           ""),
+  ("sample_category",       "signal_ggf_nonresonant_hh_wwww"),
+  ("process_name_specific", "signal_ggf_nonresonant_node_sm_hh_4v_private"),
+  ("nof_db_events",         1000000),
+  ("nof_db_files",          4000),
+  ("fsize_db",              83190363853),
+  ("xsection",              0.057547),
+  ("use_it",                True),
+  ("genWeight",             True),
+  ("comment",               "status: VALID; size: 83.19GB; nevents: 1.00M; release: 9_4_0; last modified: 2020-09-20 20:38:51"),
+])
+
+meta_dictionary["/GluGluToHHTo4V_node_1_TuneCP5_PSWeights_13TeV/RunIIFall17MiniAODv2/USER"] =  OD([
+  ("crab_string",           ""),
+  ("sample_category",       "signal_ggf_nonresonant_hh_wwww"),
+  ("process_name_specific", "signal_ggf_nonresonant_node_1_hh_4v_private"),
+  ("nof_db_events",         1000000),
+  ("nof_db_files",          4000),
+  ("fsize_db",              82235953558),
+  ("xsection",              0.057547),
+  ("use_it",                True),
+  ("genWeight",             True),
+  ("comment",               "status: VALID; size: 82.24GB; nevents: 1.00M; release: 9_4_0; last modified: 2020-09-20 16:44:34"),
+])
+
+meta_dictionary["/GluGluToHHTo4V_node_7_13TeV/RunIIFall17MiniAODv2/USER"] =  OD([
+  ("crab_string",           ""),
+  ("sample_category",       "signal_ggf_nonresonant_hh_wwww"),
+  ("process_name_specific", "signal_ggf_nonresonant_node_7_hh_4v_private"),
+  ("nof_db_events",         999999),
+  ("nof_db_files",          4000),
+  ("fsize_db",              82292366273),
+  ("xsection",              0.057547),
+  ("use_it",                True),
+  ("genWeight",             True),
+  ("comment",               "status: VALID; size: 82.29GB; nevents: 1000.00k; release: 9_4_0; last modified: 2020-09-20 18:49:32"),
+])
+
+
+# event statistics by sample category:
+# signal_ggf_nonresonant_hh_tttt: 3.00M
+# signal_ggf_nonresonant_hh_wwtt: 3.00M
+# signal_ggf_nonresonant_hh_wwww: 3.00M
+
