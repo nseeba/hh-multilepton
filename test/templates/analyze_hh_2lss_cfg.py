f0948571264523778424c2b5ddfd4d83522faf70
Christian Veelken <christian.veelken@cern.ch>
2021-02-12 09:58:59 +0200

    added apply_genPhotonFilter flag

--- test/templates/analyze_hh_2lss_cfg.py
+++ test/templates/analyze_hh_2lss_cfg.py
@@ -94,6 +94,7 @@ process.analyze_hh_2lss = cms.PSet(
     cfgMEtFilter = cms.PSet(),
     triggerWhiteList = cms.PSet(),
     apply_hadTauFakeRateSF = cms.bool(False),
+    apply_genPhotonFilter = cms.string("disabled"),
 
     fillGenEvtHistograms = cms.bool(False),
     cfgEvtYieldHistManager = cms.PSet(),
