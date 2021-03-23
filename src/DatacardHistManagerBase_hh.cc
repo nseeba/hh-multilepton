96fa7ad73e521b83e5f7616873d9b8ea3ee56f12
Christian Veelken <christian.veelken@cern.ch>
2021-03-23 16:00:41 +0200

    added option to apply NLO->NLO reweighting (this allows to reweight the SM NLO MC sample to different benchmark scenarios as NLO accuracy)

--- src/DatacardHistManagerBase_hh.cc
+++ src/DatacardHistManagerBase_hh.cc
@@ -302,7 +302,7 @@ DatacardHistManagerBase_hh::compHHReweightMap()
       if ( apply_HH_rwgt_nlo_ )
       {
         assert(HHWeightNLO_calc_);
-        HHReweight *= HHWeightNLO_calc_->getReWeight_V2(bmName, eventInfo_.gen_mHH, eventInfo_.gen_cosThetaStar, isDEBUG_);
+        HHReweight *= HHWeightNLO_calc_->getReWeight_LOtoNLO_V2(bmName, eventInfo_.gen_mHH, eventInfo_.gen_cosThetaStar, isDEBUG_);
       }
       HHReweightMap_[histogramName->first] = HHReweight;
     }
