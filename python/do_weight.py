117eb00feec7939703d624d0afea8d3cdee4ebe0
ktht <karlehataht@gmail.com>
2021-04-18 23:40:30 +0300

    Compute the normalization factor in HH LO reweighting on the fly
    https://github.com/HEP-KBFI/hh-multilepton/issues/38

--- python/do_weight.py
+++ python/do_weight.py
@@ -8,5 +8,8 @@ def load(coefFile):
     model.ReadCoefficients(coefFile)
     return model
 
+def get_norm(kl, kt, c2, cg, c2g, denominator_filename, histogram_name, model):
+    return model.getNormalization(kl, kt, c2, cg, c2g, denominator_filename, histogram_name)
+
 def evaluate_weight(kl, kt, c2, cg, c2g, mhh_gen, cost_gen, calcSumOfWeights, denominator, model):
     return model.getScaleFactor(mhh_gen, cost_gen, kl, kt, c2, cg, c2g, denominator, calcSumOfWeights)
